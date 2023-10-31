67e05c8f258f985e06fca28229be67dc10a4cf79
yashvant <yashvantyadav855@gmail.com>
2023-10-31 14:04:57 +0530

    Initial commit

---
+++ src/test/java/com/yashvant/springdownloadupload/SpringDownloadUploadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yashvant.springdownloadupload;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDownloadUploadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
